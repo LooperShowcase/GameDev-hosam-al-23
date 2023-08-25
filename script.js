c9806730a788db8e330f69c2677758b2e4225b2c
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2023-08-25 17:29:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,113 +3,104 @@ let cards = [];
 let firstCard, secondCard;
 let lockBoard = false;
 let score = 0;
-
-
-let scoreBoard = document.getElementById("score");
-scoreBoard.textContent = score;
+document.getElementById("score").textContent = score;
 
 fetch("./data/cards.json")
-    .then( (res) => res.json())
-    .then ( (data) => {
-        console.log(cards);
-        cards = [...data, ...data]
-        shuffleCards();
-        generateCards();
-    }
-)
+  .then((res) => res.json())
+  .then((data) => {
+    cards = [...data, ...data];
+    shuffleCards();
+    generateCards();
+    console.log(cards);
+  });
 
-function shuffleCards(){
-    let currentIndex = cards.length;
-    let randomIndex;
-    let temporaryValue;
+function shuffleCards() {
+  let randomIndex;
+  let currentIndex = cards.length;
+  let temp;
 
-    while (currentIndex >0){
-        randomIndex = Math.floor(Math.random()*currentIndex);
-        currentIndex -= 1;
-        temporaryValue = cards[currentIndex];
-        cards[currentIndex] = cards[randomIndex];
-        cards[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    temp = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temp;
+  }
 }
 
 function generateCards() {
-
-    for (let card of cards){
-        const cardElement = document.createElement("div");
-        cardElement.classList.add("card");
-        cardElement.setAttribute("data-name", card.name);
-        cardElement.innerHTML = `
-        <div class="front">
-            <img class="front-image" src=${card.image} />
-        </div>
-        <div class="back"></div>
-        `;
-        cardsContainer.appendChild(cardElement);
-        cardElement.addEventListener("click", flipCard);
-        cardElement.addEventListener("touchstart", flipCard);
-    }
+  for (let card of cards) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    cardElement.setAttribute("data-name", card.name);
+    cardElement.innerHTML = `
+    <div class="front">
+      <img class="front-image" src="${card.image}" />
+    </div>
+    <div class="back"></div>
+    `;
+    cardsContainer.appendChild(cardElement);
+    cardElement.addEventListener("click", flipCard);
+    cardElement.addEventListener("touchstart", flipCard);
+  }
 }
 
 function flipCard() {
-    if(lockBoard) {
-        return;
-    }
-    if (this === firstCard) {
-        return;
-    }
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-    this.classList.add("flipped");
+  this.classList.add("flipped");
 
-    if(!firstCard) {
-        firstCard = this;
-        return;
-    }
-    secondCard = this;
-    lockBoard = true;
-    checkForMatch();
-    scoreBoard.textContent = score;
+  if (!firstCard) {
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+  lockBoard = true;
+  checkForMatch();
+  document.getElementById("score").textContent = score;
 }
 
 function checkForMatch() {
-    if(firstCard.dataset.name === secondCard.dataset.name)
-        disableCards();
-    
-    else unflipCards();
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+  if (isMatch) {
+    disableCards();
+  } else {
+    unflipCards();
+  }
 }
 
-function disableCards(){
-    firstCard.removeEventListener("click", flipCard);
-    secondCard.removeEventListener("click", flipCard);
-    firstCard.removeEventListener("touchstart", flipCard);
-    secondCard.removeEventListener("touchstart", flipCard);
-    score++ ;
-    if(score === 9 )
-        startConfetti();
-    unlockBoard();
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  firstCard.removeEventListener("touchstart", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("touchstart", flipCard);
+  score++;
+  if (score === 9) startConfetti();
+  unlockBoard();
+}
+
+function unlockBoard() {
+  firstCard = null;
+  secondCard = null;
+  lockBoard = false;
 }
 
 function unflipCards() {
-    setTimeout(() => {
-        firstCard.classList.remove("flipped");
-        secondCard.classList.remove("flipped");
-        unlockBoard();
-    }, 600)
+  setTimeout(() => {
+    firstCard.classList.remove("flipped");
+    secondCard.classList.remove("flipped");
+    unlockBoard();
+  }, 1000);
 }
 
-function unlockBoard(){
-    firstCard = null;
-    secondCard = null;
-    lockBoard = false;
+function restart() {
+  shuffleCards();
+  unlockBoard();
+  score = 0;
+  document.getElementById("score").textContent = score;
+  cardsContainer.innerHTML = "";
+  generateCards();
+  stopConfetti();
 }
-
-function restart()
-{
-    shuffleCards()
-    unlockBoard()
-    score = 0
-    scoreBoard.textContent = score
-    cardsContainer.innerHTML = "";
-    generateCards()
-    stopConfetti();
-}
-
